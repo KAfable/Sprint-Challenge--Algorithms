573e8c50b61e8bbb994a4f938bde961b15bc575a
Vyraal1 <kevin.afable@gmail.com>
2020-02-21 12:37:13 -0500

    feat: bubble sort for robot MVP

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -57,6 +57,8 @@ class SortingRobot:
         """
         self._time += 1
         # Swap the held item with the list item at the robot's position
+        print(
+            f"Picking up {self._list[self._position]} and leaving behind {self._item}")
         self._item, self._list[self._position] = self._list[self._position], self._item
 
     def compare_item(self):
@@ -70,8 +72,10 @@ class SortingRobot:
         if self._item is None or self._list[self._position] is None:
             return None
         elif self._item > self._list[self._position]:
+            print(f"{self._item} is greater than {self._list[self._position]}")
             return 1
         elif self._item < self._list[self._position]:
+            print(f"{self._item} is less than {self._list[self._position]}")
             return -1
         else:
             return 0
@@ -106,18 +110,33 @@ class SortingRobot:
         #
         # can this be done recursively?
         # is the light even helpful at all?
+        print(f'''
+        \n Starting out with:
+        \n {self._item} in hand
+        ''')
+        self.swap_item()
+        for i in range(len(self._list)-1, 0, -1):
+            for j in range(0, i):
+                if self.compare_item() == -1:
+                    self.swap_item()
+                self.move_right()
+            # drop off the highest item found so far
+            self.swap_item()
+            while self.can_move_left():
+                self.move_left()
+        self.swap_item()
 
-        for item in self._list:
-            # pick up the first item
-            # compare it the next item
-            # might use a while loop to simplify
-            # while the bot can move right, compare and swap if greater
-            # this will bubble the highest to the right
+        # if it doesn't
 
-            # to save time on the way back
-            # while the robot can move left
-            ##
-            print(f"this is the item: {item}")
+        # pick up the first item
+        # compare it the next item
+        # might use a while loop to simplify
+        # while the bot can move right, compare and swap if greater
+        # this will bubble the highest to the right
+
+        # to save time on the way back
+        # while the robot can move left
+        ##
 
 
 if __name__ == "__main__":
@@ -126,6 +145,8 @@ if __name__ == "__main__":
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
          45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21]
 
     robot = SortingRobot(l)
 
